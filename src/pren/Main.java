0006c63dd140d7ec27e2352772b9dc0d47ad1762
Alexander Suter <alexander.suter@gmx.ch>
2014-11-14 15:22:25 +0100

    Versuche

--- src/pren/Main.java
+++ src/pren/Main.java
@@ -1,27 +1,11 @@
 package pren;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
 
 public class Main {
 
-   public static void eee() {
-      List<Integer> list = new ArrayList<Integer>();
-      list.add(1);
-      list.add(2);
-      list.add(3);
-      list.add(4);
-
-      ListIterator<Integer> listIterator = list.listIterator(list.size());
-      while (listIterator.hasPrevious()) {
-         System.out.println(listIterator.previous());
-      }
-   }
-
    public static void main(String[] args) {
-      eee();
+
       // String file1 = "korb-bg-white.jpg";
       String file2 = "korb-bg-yellow.jpg";
 
@@ -41,13 +25,10 @@ public class Main {
 
       // 3. Greyscale
       processor.greyscaleIt();
-      // processor.saveImage("step3-greyscale");
+      processor.saveImage("step3-greyscale");
 
       // 4. Read Pixel
-      for (int i = 0; i < processor.getPixelWidth(); i++) {
-         // System.out.println(i + ": " + processor.readPixel(200, i));
-      }
-
+      processor.alternatePixel();
    }
 
 }
--- src/pren/PrenImageProcessor.java
+++ src/pren/PrenImageProcessor.java
@@ -5,6 +5,9 @@ import ij.plugin.ContrastEnhancer;
 import ij.process.ImageConverter;
 import ij.process.ImageProcessor;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -13,6 +16,8 @@ import javax.imageio.ImageIO;
 
 public class PrenImageProcessor {
 
+   private File file;
+
    private ImagePlus image;
 
    public PrenImageProcessor(File file) {
@@ -20,8 +25,9 @@ public class PrenImageProcessor {
    }
 
    public void saveImage(String name) {
+      this.file = new File("output" + File.separator + name + ".jpg");
       try {
-         ImageIO.write(image.getBufferedImage(), "jpg", new File("output" + File.separator + name + ".jpg"));
+         ImageIO.write(image.getBufferedImage(), "jpg", this.file);
       } catch (IOException e) {
          e.printStackTrace();
       }
@@ -52,6 +58,38 @@ public class PrenImageProcessor {
       int[] rgb = new int[3];
       imp.getPixel(x, y, rgb);
       return Arrays.toString(rgb);
+   }
+
+   public void alternatePixel() {
+      BufferedImage bi;
+      try {
+         bi = ImageIO.read(this.file);
+         for (int x = 0; x < bi.getWidth(); x++) {
+            Color c = new Color(bi.getRGB(x, 300));
+            // System.out.println("red==" + c.getRed() + " green==" +
+            // c.getGreen() + "    blue==" + c.getBlue());
+            System.out.println(c.getGreen());
+         }
+
+         Graphics2D g2d = bi.createGraphics();
+         g2d.setBackground(Color.red);
+         g2d.setColor(Color.red);
+         g2d.drawLine(0, 300, bi.getWidth(), 300);
+
+         g2d.drawImage(bi, null, 0, 0);
+         ImageIO.write(bi, "JPEG", new File("gogogo.jpg"));
+         // for (int x = 0; x < bi.getWidth(); x++) {
+         // for (int y = 0; y < bi.getHeight(); y++) {
+         // Color c = new Color(bi.getRGB(x, y));
+         // System.out.println("red==" + c.getRed() + " green==" + c.getGreen()
+         // + "    blue==" + c.getBlue());
+         // }
+         // }
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+
    }
 
    public int getPixelWidth() {
